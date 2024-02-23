ade52b883f64ec9feab861ab526e9acc553e2a8a
Muhammad Rauf Qureshi <rauf@elucidate.co>
2024-02-23 19:26:03 +0100

    04 Add moisturizers structure

---
+++ cypress/e2e/MoisturizerPage.spec.ts
@@ -0,0 +1,16 @@
+import {Fixtures} from "../fixtures";
+
+describe('Shop for suncreens if the weather is above 34 degrees', () => {
+
+    beforeEach(()=> {
+        Fixtures.open('')
+    })
+
+    after(()=>{
+
+    })
+
+    it('should add two sunscreens to your cart', () => {
+
+    });
+})
