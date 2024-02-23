266abcf0e7c0f63e8c82169f4e926f7c2cd697f8
Muhammad Rauf Qureshi <rauf@elucidate.co>
2024-02-23 22:22:05 +0100

    05 🧪 E2E test case

--- cypress/e2e/MoisturizerPage.spec.ts
+++ cypress/e2e/MoisturizerPage.spec.ts
@@ -1,18 +1,22 @@
 import {Fixtures} from "../fixtures";
 import {Base} from "../objects/Base";
+import {Weathershopper} from "../support/Weathershopper";
 
-describe('Shop for suncreens if the weather is above 34 degrees', () => {
+describe('Shop for moisturizers if the weather is below 19 degrees', () => {
 
     beforeEach(()=> {
-        Fixtures.open('')
+        Fixtures.open('/moisturizer')
     })
 
     after(()=>{
 
     })
 
-    it('should add two sunscreens to your cart', () => {
-        Fixtures.checkTemperature()
-
+    it('should add two moisturizers to your cart', () => {
+        Weathershopper.assertLeastExpensiveMositurizerWithAloe()
+        Weathershopper.assertLeastExpensiveMositurizerWithAlmond()
+        cy.get(Base.productCard.cartButton).click()
+        cy.reload()
     });
+
 })
